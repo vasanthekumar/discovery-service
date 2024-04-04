d31861657f21f114c150fdcd31fa20c2d02a2a3e
Vasanth <vasanth.fine@yahoo.com>
2024-04-04 22:20:15 +0300

    initial commit.

---
+++ src/main/java/com/discovery/service/demodiscovery/DemoDiscoveryApplication.java
@@ -0,0 +1,15 @@
+package com.discovery.service.demodiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DemoDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDiscoveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/discovery/service/demodiscovery/DemoDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.discovery.service.demodiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
